3eb0117cab429ad74d026e67bd121e15c5e4dfba
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-05 20:37:53 +0200

    [add] test

---
+++ src/test/java/com/codecool/stocktraderspringboot/StocktraderspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.stocktraderspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktraderspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
